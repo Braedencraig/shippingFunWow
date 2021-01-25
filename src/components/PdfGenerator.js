49426a6c7c8f56d2d0feee8caee61780e1a76586
Braeden <bcraig@urbancoolab.com>
2021-01-24 22:45:16 -0500

    make pdf dynamic with content after purchase now

--- src/components/PdfGenerator.js
+++ src/components/PdfGenerator.js
@@ -1,139 +1,29 @@
 import React, { useEffect } from 'react';
 import { Page, Text, View, Document, StyleSheet, Image, PDFDownloadLink, Font } from '@react-pdf/renderer';
 import logo from '../logo.svg'
-// import { useStoreState } from 'easy-peasy'
-
-// const Quixote = () => (
-//   <Document>
-//     <Page style={styles.body}>
-//       <Text style={styles.title}>Don Quijote de la Mancha</Text>
-//       <Text style={styles.author}>Miguel de Cervantes</Text>
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Image
-//         style={styles.image}
-//         src="https://staging.chitchats.com/labels/shipments/p7a932d2b1.png"
-//       />
-//       <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
-//         `${pageNumber} / ${totalPages}`
-//       )} fixed />
-//     </Page>
-//   </Document>
-// );
-
-// Font.register({
-//   family: 'Oswald',
-//   src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
-// });
-
-// const styles = StyleSheet.create({
-//   body: {
-//     paddingTop: 35,
-//     paddingBottom: 65,
-//     paddingHorizontal: 35,
-//   },
-//   title: {
-//     fontSize: 24,
-//     textAlign: 'center',
-//     fontFamily: 'Oswald'
-//   },
-//   author: {
-//     fontSize: 12,
-//     textAlign: 'center',
-//     marginBottom: 40,
-//   },
-//   subtitle: {
-//     fontSize: 18,
-//     margin: 12,
-//     fontFamily: 'Oswald'
-//   },
-//   text: {
-//     margin: 12,
-//     fontSize: 14,
-//     textAlign: 'justify',
-//     fontFamily: 'Times-Roman'
-//   },
-//   image: {
-//     marginVertical: 15,
-//     marginHorizontal: 100,
-//   },
-//   header: {
-//     fontSize: 12,
-//     marginBottom: 20,
-//     textAlign: 'center',
-//     color: 'grey',
-//   },
-//   pageNumber: {
-//     position: 'absolute',
-//     fontSize: 12,
-//     bottom: 30,
-//     left: 0,
-//     right: 0,
-//     textAlign: 'center',
-//     color: 'grey',
-//   },
-// });
-
 
 // Create Document Component
 const PdfGenerator = ({todo, todoInfo}) => {
   console.log(todo.length)
   console.log(todoInfo)
 
-  console.log(todo[0])
-
   const Quixote = () => {
-  return (
-    <Document>
-      {/* <Page style={styles.body}> */}
-        {/* <Text style={styles.title}>Don Quijote de la Mancha</Text> */}
-        {todo.map((img, i) => {
-          return (
-            <Page style={styles.body}>
-            <Image
-              style={styles.image}
-              src={todo[i]}
-            />
-            <Text style={styles.author}>Miguel de Cervantes</Text>
-            <Text style={styles.author}>Miguel de Cervantes</Text>
-            <Text style={styles.author}>Miguel de Cervantes</Text>
-            <Text style={styles.author}>Miguel de Cervantes</Text>
-            <Text style={styles.author}>Miguel de Cervantes</Text>
-            </Page>
-          )
-        })}
-        {/* <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
-          `${pageNumber} / ${totalPages}`
-        )} fixed /> */}
-      {/* </Page> */}
-    </Document>
-  )
+    return (
+      <Document>
+          {todo.map((img, i) => {
+            return (
+              <Page style={styles.body}>
+              <Image
+                style={styles.image}
+                src={todo[i]}
+              />
+              <Text style={styles.author}>Miguel de Cervantes</Text>
+              <Text style={styles.author}>Miguel de Cervantes</Text>
+              </Page>
+            )
+          })}
+      </Document>
+    )
   };
   
   Font.register({
@@ -171,6 +61,7 @@ const PdfGenerator = ({todo, todoInfo}) => {
     image: {
       marginVertical: 15,
       marginHorizontal: 100,
+      transform: 'rotate(90deg)'
     },
     header: {
       fontSize: 12,
