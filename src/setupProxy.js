ef50e751f1b23a0c86712fb856dcd929a92607b8
Braeden Craig <braeden.craig@gmail.com>
2023-04-08 15:48:18 -0400

    update api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,14 @@
 const createProxyMiddleware = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   app.use(
     createProxyMiddleware("/oauth_token", {
       changeOrigin: true,
